dc75f2d0bebf28e8e8fe2082c11cf2256b0c3212
Virink <virink@outlook.com>
2016-01-07 16:12:46 +0800

    x

---
+++ vMysqlMonitoring/MysqlMonitoring.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace MysqlMonitoringMain
+{
+    partial class MysqlMonitoring
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MysqlMonitoring));
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_user = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_host = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.CopyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txt_count = new System.Windows.Forms.Label();
+            this.txt_break = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_searchkey = new System.Windows.Forms.TextBox();
+            this.btn_auto = new System.Windows.Forms.Button();
+            this.txt_time = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Location = new System.Drawing.Point(391, 12);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.Size = new System.Drawing.Size(123, 21);
+            this.txt_pass.TabIndex = 7;
+            this.txt_pass.Text = "root";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(337, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "密码：";
+            // 
+            // txt_user
+            // 
+            this.txt_user.Location = new System.Drawing.Point(243, 12);
+            this.txt_user.Name = "txt_user";
+            this.txt_user.Size = new System.Drawing.Size(81, 21);
+            this.txt_user.TabIndex = 8;
+            this.txt_user.Text = "root";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "用户：";
+            // 
+            // txt_host
+            // 
+            this.txt_host.Location = new System.Drawing.Point(69, 12);
+            this.txt_host.Name = "txt_host";
+            this.txt_host.Size = new System.Drawing.Size(107, 21);
+            this.txt_host.TabIndex = 9;
+            this.txt_host.Text = "localhost";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "主机：";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(527, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "初始化";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(615, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "获 取";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 40);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1160, 492);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "执行过程";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 17);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.ShowCellErrors = false;
+            this.dataGridView1.ShowRowErrors = false;
+            this.dataGridView1.Size = new System.Drawing.Size(1154, 472);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CopyToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(105, 26);
+            // 
+            // CopyToolStripMenuItem
+            // 
+            this.CopyToolStripMenuItem.Name = "CopyToolStripMenuItem";
+            this.CopyToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.CopyToolStripMenuItem.Text = "复 制";
+            this.CopyToolStripMenuItem.Click += new System.EventHandler(this.CopyToolStripMenuItem_Click);
+            // 
+            // txt_count
+            // 
+            this.txt_count.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txt_count.AutoSize = true;
+            this.txt_count.Location = new System.Drawing.Point(203, 541);
+            this.txt_count.Name = "txt_count";
+            this.txt_count.Size = new System.Drawing.Size(47, 12);
+            this.txt_count.TabIndex = 12;
+            this.txt_count.Text = "行数：0";
+            // 
+            // txt_break
+            // 
+            this.txt_break.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txt_break.AutoSize = true;
+            this.txt_break.Location = new System.Drawing.Point(15, 541);
+            this.txt_break.Name = "txt_break";
+            this.txt_break.Size = new System.Drawing.Size(41, 12);
+            this.txt_break.TabIndex = 13;
+            this.txt_break.Text = "断点：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(853, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "搜索：";
+            // 
+            // txt_searchkey
+            // 
+            this.txt_searchkey.Location = new System.Drawing.Point(900, 12);
+            this.txt_searchkey.Name = "txt_searchkey";
+            this.txt_searchkey.Size = new System.Drawing.Size(269, 21);
+            this.txt_searchkey.TabIndex = 14;
+            this.txt_searchkey.TextChanged += new System.EventHandler(this.txt_searchkey_TextChanged);
+            // 
+            // btn_auto
+            // 
+            this.btn_auto.BackColor = System.Drawing.Color.White;
+            this.btn_auto.Location = new System.Drawing.Point(703, 11);
+            this.btn_auto.Name = "btn_auto";
+            this.btn_auto.Size = new System.Drawing.Size(94, 23);
+            this.btn_auto.TabIndex = 15;
+            this.btn_auto.Text = "开始自动获取";
+            this.btn_auto.UseVisualStyleBackColor = false;
+            this.btn_auto.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txt_time
+            // 
+            this.txt_time.Location = new System.Drawing.Point(803, 11);
+            this.txt_time.Name = "txt_time";
+            this.txt_time.Size = new System.Drawing.Size(28, 21);
+            this.txt_time.TabIndex = 16;
+            this.txt_time.Text = "1";
+            this.txt_time.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(837, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(11, 12);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "s";
+            // 
+            // MysqlMonitoring
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1184, 562);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_time);
+            this.Controls.Add(this.btn_auto);
+            this.Controls.Add(this.txt_searchkey);
+            this.Controls.Add(this.txt_break);
+            this.Controls.Add(this.txt_count);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_user);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_host);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1200, 200);
+            this.Name = "MysqlMonitoring";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MysSQL 执行语句监测 general_log版本 By Virink 【借鉴Seay法师插件源码】";
+            this.Load += new System.EventHandler(this.F_MysqlMonitoring_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_pass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_user;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_host;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label txt_count;
+        private System.Windows.Forms.Label txt_break;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem CopyToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_searchkey;
+        private System.Windows.Forms.Button btn_auto;
+        private System.Windows.Forms.TextBox txt_time;
+        private System.Windows.Forms.Label label5;
+    }
+}
